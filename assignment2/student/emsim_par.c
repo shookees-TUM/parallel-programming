7b8062483073dc9db43f084b722211a633f4af60
Paulius Šukys <paul.sukys@gmail.com>
2016-05-09 18:43:07 +0200

    Initial assignment 2 file

---
+++ assignment2/student/emsim_par.c
@@ -0,0 +1,12 @@
+#include "emsim.h"
+#include <pthread.h>
+
+void playGroups(team_t* teams, int numWorker)
+{
+ // put your code here
+}
+
+void playFinalRound(int numGames, team_t** teams, team_t** successors, int numWorker)
+{
+ // put your code here
+} 
