db022811b0b13a4eb9c7ecfc7511cf15afca5ae1
Paulius Šukys <paul.sukys@gmail.com>
2016-05-24 07:36:23 +0200

    Initial commit for blank assignment3's parallel file

---
+++ assignment3/student/emsim_par.c
@@ -0,0 +1,12 @@
+#include "emsim.h"
+#include <omp.h>
+
+void playGroups(team_t* teams)
+{
+ // put your code here
+}
+
+void playFinalRound(int numGames, team_t** teams, team_t** successors)
+{
+ // put your code here
+} 
