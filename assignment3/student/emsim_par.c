9f431f44bb20629eaf08cd7cfc6c241a56f4ae98
Paulius Šukys <paul.sukys@gmail.com>
2016-05-30 13:53:52 +0200

    Copied solution from sequential code and added OpenMPI directives before loops: playGroups() - parallelize for cycle with private g, i, j, goalsI, and goalsJ; and dynamically schedule it. playFinalRoun() - parallelize for cycle with private team1, team2, i, goals1, goals2 and dynamic scheduling

--- assignment3/student/emsim_par.c
+++ assignment3/student/emsim_par.c
@@ -1,12 +1,55 @@
 #include "emsim.h"
 #include <omp.h>
 
-void playGroups(team_t* teams)
-{
- // put your code here
+// play all (36) group games
+void playGroups(team_t* teams) {
+  static const int cNumTeamsPerGroup = NUMTEAMS / NUMGROUPS;
+  int g, i, j, goalsI, goalsJ;
+
+  #pragma omp parallel for private(g, i, j, goalsI, goalsJ) schedule(dynamic, 1)
+  for (g = 0; g < NUMGROUPS; ++g) {
+    for (i =  g * cNumTeamsPerGroup; i < (g+1) * cNumTeamsPerGroup; ++i) {
+      for (j = (g+1) * cNumTeamsPerGroup - 1; j > i; --j) {
+
+        // team i plays against team j in group g
+        playGroupMatch(g, teams + i, teams + j, &goalsI, &goalsJ);
+        teams[i].goals += goalsI - goalsJ;
+        teams[j].goals += goalsJ - goalsI;
+        if (goalsI > goalsJ)
+          teams[i].points += 3;
+        else if (goalsI < goalsJ)
+          teams[j].points += 3;
+        else {
+          teams[i].points += 1;
+          teams[j].points += 1;
+        }
+      }
+    }
+  }
 }
 
-void playFinalRound(int numGames, team_t** teams, team_t** successors)
-{
- // put your code here
-} 
+// play a specific final round 
+void playFinalRound(int numGames, team_t** teams, team_t** successors){
+  team_t* team1;
+  team_t* team2;
+  int i, goals1 = 0, goals2 = 0;
+
+  #pragma omp parallel for private(team1, team2, i, goals1, goals2) schedule(dynamic, 1)
+  for (i = 0; i < numGames; ++i) {
+    team1 = teams[i*2];
+    team2 = teams[i*2+1];
+    playFinalMatch(numGames, i, team1, team2, &goals1, &goals2);
+
+    if (goals1 > goals2)
+      successors[i] = team1;
+    else if (goals1 < goals2)
+      successors[i] = team2;
+    else {
+      playPenalty(team1, team2, &goals1, &goals2);
+      if (goals1 > goals2)
+        successors[i] = team1;
+      else
+        successors[i] = team2;
+    }
+  }
+}
