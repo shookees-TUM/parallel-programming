700eb1a904b723df406d669ddaf8de4f2e631113
Paulius Šukys <paul.sukys@gmail.com>
2016-07-05 15:22:37 +0200

    Since for S1 and S2 there was a dependency on first depth level, I changed the loop ordering. Added  casual OMP parallelization

--- assignment7/student/loop_fission_par.c
+++ assignment7/student/loop_fission_par.c
@@ -10,17 +10,21 @@
  */
 
 void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned long **d, int N, int num_threads) {
-
+    #pragma omp parallel num_threads(num_threads)
+    {
+    #pragma omp for
 	for (int j = 1; j < N; j++) {
 		for (int i = 1; i < N; i++) {
 			c[i][j] = 3 * d[i][j];                       // S3
 			d[i][j]   = 2 * c[i-1][j];                   // S4
 		}
 	}
-    for (int j = 1; j < N; j++) {
-        for (int i = 1; i < N; i++) {
+    #pragma omp for
+    for (int i = 1; i < N; i++) {
+         for (int j = 1; j < N; j++) {
             a[i][j]   = a[i][j] * b[i][j+1]+d[i][j-1];   // S1
             b[i][j] = 2 * a[i][j] * c[i - 1][j];         // S2
         }
+    }
     }
 }
