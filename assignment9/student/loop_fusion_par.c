a7cd86cbc03dfd640a60fddb6794ce9a49a65b12
Paulius Šukys <paul.sukys@gmail.com>
2016-07-05 17:05:18 +0200

    Both cycles have no variation in i cycle, thus can be isolated in that level.

--- assignment9/student/loop_fusion_par.c
+++ assignment9/student/loop_fusion_par.c
@@ -2,15 +2,12 @@
 
 void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned long **d, int N, int num_threads) {
 
-	// perform loop fusion and use OpenMP directives appropriately. 
 
 	for (int i = 1; i < N; i++) {
 		for (int j = 1; j < N; j++) {
 			a[i][j] = 2 * b[i][j] +d[i][j];
 		}
-	}
 
-	for (int i = 1; i < N; i++) {
 		for (int j = 1; j < N; j++) {
 			d[i][j + 1] = a[i][j + 1] * c[i][j    ];
 			c[i][j - 1] = a[i][j - 1] - a[i][j + 1];
