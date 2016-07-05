71cc35147a0d1d7d171353b6d80d50d21f4e9eec
Paulius Šukys <paul.sukys@gmail.com>
2016-07-05 17:11:44 +0200

    shifted second sub-loop to left by 1 and added compensation after. This is important, as I will have to unify the dependency on a[i][j]

--- assignment9/student/loop_fusion_par.c
+++ assignment9/student/loop_fusion_par.c
@@ -2,15 +2,18 @@
 
 void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned long **d, int N, int num_threads) {
 
-
 	for (int i = 1; i < N; i++) {
 		for (int j = 1; j < N; j++) {
 			a[i][j] = 2 * b[i][j] +d[i][j];
 		}
 
-		for (int j = 1; j < N; j++) {
-			d[i][j + 1] = a[i][j + 1] * c[i][j    ];
-			c[i][j - 1] = a[i][j - 1] - a[i][j + 1];
+		//shifted to left by 1
+		for (int j = 2; j < N; j++) {
+			d[i][j    ] = a[i][j    ] * c[i][j - 1];
+			c[i][j - 2] = a[i][j - 2] - a[i][j    ];
 		}
+		// add the missing part of the loop
+		d[i][N] = a[i][N] * c[i][N - 1];
+		c[i][N - 2] = a[i][N - 2] - a[i][N];
 	}
 }
