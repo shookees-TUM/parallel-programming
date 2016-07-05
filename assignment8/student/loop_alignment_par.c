940a2390c128f0ef94518b39f7d2c9a803cbaa4d
Paulius Šukys <paul.sukys@gmail.com>
2016-07-05 15:24:09 +0200

    Added initial version of assignment8

---
+++ assignment8/student/loop_alignment_par.c
@@ -0,0 +1,23 @@
+#include <omp.h>
+
+void compute(unsigned long *a, unsigned long *b, unsigned long *c, unsigned long *d, int N, int num_threads) {
+
+	// perform loop alignment to transform this loop and parallelize it with OpenMP	
+
+    a[1] = d[1] * b[1];
+    c[0] = a[1] * d[1];
+    #pragma omp parallel num_threads(num_threads)
+    {
+    #pragma omp for
+	for (int i = 2; i < N - 1; i++) {
+		
+        b[i] = 2 * c[i - 1];
+
+		a[i] = d[i] * b[i];
+		
+		c[i - 1] = a[i] * d[i];
+		
+	}
+    }
+    b[N] = 2 * c[N - 1];
+}
